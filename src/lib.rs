f39505a0c67b92c2791e2495e297ce89a2581b50
题叶 <jiyinyiyong@gmail.com>
2026-08-22 23:06:04 +0800

    chore: upgrade graphviz to Calcit 0.13.31 (#6)

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,26 @@ use cirru_edn::Edn;
 
 #[unsafe(no_mangle)]
 pub fn abi_version() -> String {
-  String::from("0.0.9")
+    String::from("0.0.9")
 }
 
 #[unsafe(no_mangle)]
 pub fn edn_version() -> String {
-  cirru_edn::version().to_owned()
+    cirru_edn::version().to_owned()
 }
 
 #[unsafe(no_mangle)]
 pub fn render_dot_file(args: Vec<Edn>) -> Result<Edn, String> {
-  if args.len() == 1 {
-    if let Edn::Map(_data) = &args[0] {
-      Ok(Edn::Str("TODO".into()))
+    if args.len() == 1 {
+        if let Edn::Map(_data) = &args[0] {
+            Ok(Edn::Str("TODO".into()))
+        } else {
+            Err(format!(
+                "render-dot-file! expected 1 piece of data, got {:?}",
+                args
+            ))
+        }
     } else {
-      Err(format!("render-dot-file! expected 1 piece of data, got {:?}", args))
+        Err(format!("render-dot-file! expected 1 arg, got {:?}", args))
     }
-  } else {
-    Err(format!("render-dot-file! expected 1 arg, got {:?}", args))
-  }
 }
